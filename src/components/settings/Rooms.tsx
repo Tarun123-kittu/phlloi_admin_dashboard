c389f4c94e0f2c5e61e917fe8988b09f7728f419
harmeetkaursohi95@gmail.com <harmeetkaursohi95@gmail.com>
2024-11-27 10:47:13 +0530

    changes

--- src/components/settings/Rooms.tsx
+++ src/components/settings/Rooms.tsx
@@ -4,6 +4,9 @@ import RoomsCard from "./RoomsCard"
 const Rooms = () => {
   return (
     <div>
+        <div>
+            <h1>Rooms List</h1>
+        </div>
         <RoomsCard />
     </div>
   )

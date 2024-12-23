35ed9e465f91fb51d4837135c810e66c292c5f5d
harmeetkaursohi95@gmail.com <harmeetkaursohi95@gmail.com>
2024-12-23 15:17:49 +0530

    base url addes to main

--- src/config/app_config.js
+++ src/config/app_config.js
@@ -1,3 +1,3 @@
 export const API_CONFIG = {
-    BASE_URL: "https://services.phloii.com/api/v1/"
-}
+  BASE_URL: process.env.REACT_PUBLIC_API_URL || "https://dev.phloii.com/api/v1", // Default to development URL if not set
+};

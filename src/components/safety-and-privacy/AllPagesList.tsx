a45d9278011b8adaa65846dc2f02fd0f8405c7e7
Tarun123-kittu <tarun.gautam@ultivic.com>
2024-12-23 14:03:18 +0530

    side bar updated

--- src/components/safety-and-privacy/AllPagesList.tsx
+++ src/components/safety-and-privacy/AllPagesList.tsx
@@ -64,7 +64,7 @@ const AllPagesList = () => {
             <div className="relative overflow-x-auto rounded-[10px]  bg-cardBg p-2 shadow-1 dark:border-dark-3 dark:bg-gray-dark dark:shadow-card">
                 <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700   dark:bg-gray-700 dark:text-gray-400">
-                        <tr className='border-b dark[#fdfdfd3d] dark:[#fdfdfd3d]'>
+                        <tr className='border-b border-[#fdfdfd3d] dark:[#fdfdfd3d]'>
                             <th scope="col" className="px-6 py-3 text-sm font-normal text-white">
                                 Section name
                             </th>

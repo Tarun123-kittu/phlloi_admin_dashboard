6ea8c482ee36c9879ce8dace581a84932f61db67
Tarun123-kittu <tarun.gautam@ultivic.com>
2024-12-23 17:15:29 +0530

    added bg

--- src/components/Layouts/DefaultLaout.tsx
+++ src/components/Layouts/DefaultLaout.tsx
@@ -25,7 +25,7 @@ export default function DefaultLayout({
         {/* <!-- ===== Sidebar End ===== --> */}
 
         {/* <!-- ===== Content Area Star ===== --> */}
-        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden bg-black">
           {/* <!-- ===== Header Star ===== --> */}
           <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
           {/* <!-- ===== Header End ===== --> */}

5dc71a93343a7940d8d28cd7c312218a658ca90f
harmeetkaursohi95@gmail.com <harmeetkaursohi95@gmail.com>
2024-12-26 17:09:36 +0530

    changes asved

--- next.config.mjs
+++ next.config.mjs
@@ -6,25 +6,36 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "cdn.sanity.io",
-        port: ""
+        port: "",
       },
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
-        port: ""
+        port: "",
       },
       {
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
-        port: ""
+        port: "",
       },
       {
         protocol: "https",
         hostname: "pub-b7fd9c30cdbf439183b75041f5f71b92.r2.dev",
-        port: ""
-      }
-    ]
-  }
+        port: "",
+      },
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: (() => {
+      const isProduction = process.env.VERCEL_ENV === "production";
+      const isPreview = process.env.VERCEL_ENV === "preview";
+      return isProduction
+        ? "https://services.phloii.com/api/v1/" // Production URL
+        : isPreview
+        ? "https://staging.phloii.com/api/v1/" // Preview URL
+        : "https://dev.phloii.com/api/v1/";    // Development URL
+    })(),
+  },
 };
 
 export default nextConfig;

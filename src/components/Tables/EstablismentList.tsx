d30a1382e6d218cc3f83917566bd492ecbef03d2
hankish1995 <babita.gurung@ultivic.com>
2025-02-06 19:44:05 +0530

    delete button alignment updated

--- src/components/Tables/EstablismentList.tsx
+++ src/components/Tables/EstablismentList.tsx
@@ -232,14 +232,14 @@ const EstablishmentList = () => {
                   <td
                     className={`border-[#fdfdfd3d] border-b px-4 py-4 dark:border-dark-3 `}
                     title="View Details"
-                    onClick={() => router.push(`/establishment-view/${data?._id}`)}
+                   
                   >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 cursor-pointer">
+                     <div className="flex items-center gap-3">
+                    <svg  onClick={() => router.push(`/establishment-view/${data?._id}`)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 cursor-pointer">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                       <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                     </svg>
-                  </td>
-                  <td>
+                 
                     <div
                       title="Delete Section"
                       className="cursor-pointer"
@@ -263,6 +263,7 @@ const EstablishmentList = () => {
                           strokeLinejoin="round"
                         />
                       </svg>
+                    </div>
                     </div>
                   </td>
                 </tr>
